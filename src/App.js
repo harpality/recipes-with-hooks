b0e287861e6f5ec3b7c6d2b135caee7934116737
Speedway <harpalassi@gmail.com>
2019-04-26 15:39:25 -0500

    creating API call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${
+    process.env.REACT_APP_ID
+  }&app_key=${process.env.REACT_APP_KEY}`;
+
+  useEffect(()=> {
+    console.log('Effect has been run')
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;

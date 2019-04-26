1d773e26b2a35e88600de47927b35ba52690775c
Speedway <harpalassi@gmail.com>
2019-04-26 17:36:24 -0500

    rendering API data to page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React, {useEffect, useState} from "react";
-import "./App.css";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import Recipe from "./components/Recipe";
 
 const App = () => {
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${
-    process.env.REACT_APP_ID
-  }&app_key=${process.env.REACT_APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
-  useEffect(()=> {
-    console.log('Effect has been run')
-  })
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${
+        process.env.REACT_APP_KEY
+      }`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const updateSearch = event => {
+    setSearch(event.target.value);
+  };
+
+  const getSearch = event => {
+    event.preventDefault();
+    setQuery(search);
+  };
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search" type="submit">
           Search
         </button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
